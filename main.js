89e66e686ada85a83205f1c937fda7341ee43189
nickjj7 <116288744+nickjj7@users.noreply.github.com>
2024-03-25 09:43:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+//Código omitido
+
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
         
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+            
         }
         botoes[i].classList.add("ativo");
     }
