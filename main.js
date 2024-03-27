f5b3a272d8e41212afe90924ea0156d650595dc4
nickjj7 <116288744+nickjj7@users.noreply.github.com>
2024-03-27 08:35:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-//Código omitido
 
 
 for(let i=0;i <botoes.length;i++){
@@ -17,3 +16,7 @@ for(let i=0;i <botoes.length;i++){
 
 }
 
+const contadores = document.querySelectorAll(".contador"); 
+const tempoObjetivo1 = new Date("2024-10-05"); 
+
+contadores[0].textContent = tempo0bjetivo1; 
